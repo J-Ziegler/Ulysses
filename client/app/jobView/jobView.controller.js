a9f0f5e01224dab24860aca31e933fe4425e7a7c
raymo172 <raymo172@morris.umn.edu>
2016-04-17 16:07:03 -0500

    Luke: Added basic stuff to the Job View page, but it doesn't seem to be working. Nothing from the html is appearing on the page.

--- client/app/jobView/jobView.controller.js
+++ client/app/jobView/jobView.controller.js
@@ -1,14 +1,44 @@
 'use strict';
-(function(){
+(function() {
 
-function JobViewComponent($scope) {
-  $scope.message = 'Hello';
-}
+  class jobViewController {
 
-angular.module('ulyssesCreatorsApp')
-  .component('jobView', {
-    templateUrl: 'app/jobView/jobView.html',
-    controller: JobViewComponent
-  });
+    constructor($http, $scope, socket) {
+      this.$http = $http;
+      this.volunteers = [];
+      this.jobs = [];
+      this.schedules = [];
+      this.selected = "";
+
+      $http.get('/api/volunteers').then(response => {
+        this.volunteers = response.data;
+        socket.syncUpdates('volunteer', this.volunteers);
+      });
+
+      $http.get('/api/jobs').then(response => {
+        this.jobs = response.data;
+        socket.syncUpdates('job', this.jobs);
+      });
+
+      $http.get('/api/schedules').then(response => {
+        this.schedules = response.data;
+        socket.syncUpdates('schedule', this.schedules);
+      });
+    }
+
+    expandDetails(item){
+      if (this.selected === "") {
+        this.selected = item['_id']
+      } else {
+        this.selected = "";
+      }
+      item.show = !item.show;
+    };
+    
+
+  }
+
+  angular.module('ulyssesCreatorsApp')
+    .controller('jobViewController', jobViewController);
 
 })();
